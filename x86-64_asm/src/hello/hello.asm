9831e5e3539798208e4b0c63b8c660236f1ca906
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-02-14 12:31:12 +0800

    Add macros, for some reason

--- x86-64_asm/src/hello/hello.asm
+++ x86-64_asm/src/hello/hello.asm
@@ -5,13 +5,21 @@ section .data
 section .text
     global _start
 
-_start:
+%macro print 2
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
+    mov rsi, %1
+    mov rdx, %2
     syscall
+%endmacro
 
+%macro exit 0
     mov rax, 60
     xor rdi, rdi
     syscall
+%endmacro
+
+_start:
+    print msg, len
+
+    exit
